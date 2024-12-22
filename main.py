ba7ff76c37225b9da708d17d504376deb53e509f
Harsh Narayan Jha <harshnj@proton.me>
2024-12-22 20:22:04 +0530

    new features and model training

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # %% 1. Import Libraries
 import pandas as pd
+import numpy as np
 
 # %% 2. Import Data
 taxi_oct_2024 = pd.read_parquet("./data/yellow_tripdata_2024-10.parquet")
@@ -171,8 +172,196 @@ print(taxi_data_prepared.shape)
 # %% Cell 20
 
 # Note down categorical and numerical features
-categorical_columns = ["PULocationID", "transaction_date", "transaction_month", "transaction_day", "transaction_hour"]
-numerical_columns = ["trip_distance", "total_distance"]
+categorical_columns = [
+    "PULocationID",
+    "transaction_date",
+    "transaction_month",
+    "transaction_day",
+    "transaction_hour",
+]
+numerical_columns = ["trip_distance", "total_amount"]
 all_needed_columns = categorical_columns + numerical_columns
 
 # %% Cell 21
+
+main_taxi_df: pd.DataFrame = pd.DataFrame(taxi_data_prepared[all_needed_columns])
+print(main_taxi_df.shape)
+main_taxi_df.head()
+
+# %% Cell 22
+
+# Now, Aggregate data points
+# Now it's a good time to think about what we want to predict. We need to transform our data to a certain format
+
+taxi_grouped_by_region = main_taxi_df.groupby(categorical_columns).mean().reset_index()
+taxi_grouped_by_region["count_of_transactions"] = (
+    main_taxi_df.groupby(categorical_columns).count().reset_index()["total_amount"]
+)
+print(taxi_grouped_by_region.shape)
+taxi_grouped_by_region.head()
+
+# %% Cell 23
+
+taxi_grouped_by_region["trip_distance"].hist(bins=100, figsize=(10, 5))
+
+# %% Cell 24
+
+taxi_grouped_by_region["total_amount"].hist(bins=100, figsize=(10, 5))
+
+# %% Cell 6. Benchmark Model
+
+data_from_benchmark_model = taxi_grouped_by_region.copy()
+
+# %% Cell 26
+categorical_features_benchmark = [
+    "PULocationID",
+    "transaction_month",
+    "transaction_day",
+    "transaction_hour",
+]
+# do NOT include trip_distance here, since it directly coorelates with total_amount, and that's cheating actually!
+input_features_benchmark = categorical_features_benchmark
+target_feature_benchmark = "total_amount"
+
+# %% Cell 6.1. Train Test Split
+from sklearn.model_selection import train_test_split  # noqa: E402
+
+X_bench = data_from_benchmark_model[input_features_benchmark]
+y_bench = data_from_benchmark_model[target_feature_benchmark]
+
+# one-hot encode
+X_bench = pd.get_dummies(X_bench)
+
+X_train_b, X_test_b, y_train_b, y_test_b = train_test_split(
+    X_bench, y_bench, test_size=0.2, random_state=50
+)
+
+# %% Cell 6.2. Fit a model into the data
+from sklearn.tree import DecisionTreeRegressor  # noqa: E402
+
+tree = DecisionTreeRegressor(max_depth=10)
+tree.fit(X_train_b, y_train_b)
+
+# %% Cell 6.3. Model evalution
+
+model_at_hand = tree
+
+y_pred_b = model_at_hand.predict(X_test_b)
+
+from sklearn.metrics import mean_absolute_error  # noqa: E402
+from sklearn.metrics import mean_squared_error  # noqa: E402
+from sklearn.metrics import r2_score  # noqa: E402
+
+print("mean absolute error", mean_absolute_error(y_test_b, y_pred_b))
+print("mean squared error", mean_squared_error(y_test_b, y_pred_b))
+print("root mean squared error", np.sqrt(mean_squared_error(y_test_b, y_pred_b)))
+print("r2", r2_score(y_test_b, y_pred_b))
+
+# %% Cell 30
+data = {"true": y_test_b, "pred": y_pred_b}
+results = pd.DataFrame(data)
+
+results.plot(figsize=(15, 8), kind="scatter", x="true", y="pred")
+
+# %% 7. Feature Engineering
+
+data_with_new_features: pd.DataFrame = taxi_grouped_by_region.copy()
+
+# %% 7.1 Date-related features
+data_with_new_features["transaction_week_day"] = data_with_new_features[
+    "transaction_date"
+].dt.weekday
+data_with_new_features["weekend"] = data_with_new_features[
+    "transaction_week_day"
+].apply(lambda x: True if x in (5, 6) else False)
+
+# %% Cell 32
+
+from pandas.tseries.holiday import USFederalHolidayCalendar  # noqa: E402
+
+cal = USFederalHolidayCalendar()
+holidays = pd.to_datetime(cal.holidays(start="2024", end="2025").date)
+data_with_new_features["is_holiday"] = data_with_new_features["transaction_date"].isin(
+    holidays
+)
+
+print(data_with_new_features["weekend"].value_counts())
+print(data_with_new_features["is_holiday"].value_counts())
+data_with_new_features.head()
+
+# %% 7.2 Borough Information
+
+zone_lookup = pd.read_csv("data/taxi_zone_lookup.csv")
+zone_lookup = zone_lookup[["LocationID", "Borough"]]
+zone_lookup["LocationID"] = zone_lookup["LocationID"].astype(str)
+zone_lookup.head()
+
+# %% Cell 34
+data_with_new_features = data_with_new_features.merge(
+    zone_lookup, left_on="PULocationID", right_on="LocationID", how="left"
+)
+data_with_new_features.drop("LocationID", axis=1, inplace=True)
+data_with_new_features.head()
+
+# %% Cell 35
+data_with_new_features["Borough"].value_counts()
+
+# %% 7.3 Weather related information
+
+# Don't have the weather dataset
+
+# %% 8. Model Training
+
+data_for_model = data_with_new_features.copy()
+
+# %% Cell 38
+
+categorical_features = [
+    "PULocationID",
+    "transaction_month",
+    "transaction_day",
+    "transaction_hour",
+    "transaction_week_day",
+    "weekend",
+    "is_holiday",
+    "Borough",
+]
+input_features = categorical_features
+target_feature = "total_amount"
+
+# %% 8.1 Train Test Split
+
+from sklearn.model_selection import train_test_split  # noqa: E402
+
+X = data_for_model[input_features]
+y = data_for_model[target_feature]
+
+# one-hot encode
+X = pd.get_dummies(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=80
+)
+# %% 8.2 Decision Tree
+from sklearn.tree import DecisionTreeRegressor  # noqa: E402
+
+tree = DecisionTreeRegressor(max_depth=10)
+tree.fit(X_train, y_train)
+# %% Cell 41
+model_at_hand = tree
+
+y_pred = model_at_hand.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error  # noqa: E402
+from sklearn.metrics import mean_squared_error  # noqa: E402
+from sklearn.metrics import r2_score  # noqa: E402
+
+print("mean absolute error", mean_absolute_error(y_test, y_pred))
+print("mean squared error", mean_squared_error(y_test, y_pred))
+print("root mean squared error", np.sqrt(mean_squared_error(y_test, y_pred)))
+print("r2", r2_score(y_test, y_pred))
+# %% Cell 42
+data = {"true": y_test_b, "pred": y_pred_b}
+results = pd.DataFrame(data)
+
+results.plot(figsize=(20, 10), kind="scatter", x="true", y="pred")
